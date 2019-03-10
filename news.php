a44d4fd98f91e9aa31d81266df9de58bcb46db19
Andou <fantasy.reactor@gmail.com>
2019-03-10 23:34:33 +0700

    template is finished. TODO: create backend requests for news

--- news.php
+++ news.php
@@ -28,7 +28,7 @@ border: 1px solid #c8c8c8;
 
 </style>
 <link rel='stylesheet' id='wpProQuiz_front_style-css'  href='/resources/wpProQuiz_front.min.css?ver=0.37' type='text/css' media='all' />
-<link rel='stylesheet' id='style-css'  href='/resources/style.css?ver=4.6.13' type='text/css' media='all' />
+<link rel='stylesheet' id='style-css'  href='/resources/style.css' type='text/css' media='all' />
 <link rel='stylesheet' id='style-bootstrap-css'  href='/resources/bootstrap.min.css?ver=4.6.13' type='text/css' media='all' />
 <link rel='stylesheet' id='style-owl-slider-css'  href='/resources/owl.carousel.css?ver=4.6.13' type='text/css' media='all' />
 <link rel='stylesheet' id='style-owl-slider-theme-css'  href='/resources/owl.theme.css?ver=4.6.13' type='text/css' media='all' />
@@ -127,7 +127,7 @@ border: 1px solid #c8c8c8;
                   <div class='col-md-12'>
                      <ul id="menu-%d0%b3%d0%bb%d0%b0%d0%b2%d0%bd%d0%be%d0%b5-%d0%bc%d0%b5%d0%bd%d1%8e" class=""><li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-30 current_page_item menu-item-31"><a href="/resources/">Главная</a></li>
 <li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a href="/resources/">О мик</a></li>
-<li id="menu-item-15" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-15"><a href="news.php">Новости</a></li>
+<li id="menu-item-15" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-15"><a href="/resources/">Новости</a></li>
 <li id="menu-item-454" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-454"><a href="/resources/">Документы</a></li>
 <li id="menu-item-217" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-217"><a href="/resources/%d0%b3%d0%be%d0%bb%d0%be%d1%81%d0%be%d0%b2%d0%b0%d0%bd%d0%b8%d1%8f/">Голосования</a></li>
 <li id="menu-item-854" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-854"><a href="/resources/">&#171;Гражданин&#187;</a></li>
@@ -140,107 +140,49 @@ border: 1px solid #c8c8c8;
           </div>
       </div>
   </header>    <section>
-      <div class='container'>
+    <div class="container">
+  <div class="row category-header">
+    <div class="col-sm-8">
+            <h1 class="category-head">Новости</h1>
+    </div>
+    <div class="col-sm-4">
+      <p class="breadcrumbs"><span class="white-bg-spn"><a href="index.php">Главная</a>  /  Новости
+    </span></p></div>
+  </div>
+  <?php
+$jsonurl = "http://0.0.0.0:8081/news";
+$json = file_get_contents($jsonurl);
+$result = json_decode($json, true);
+for ($i = 0; isset($result[$i]); $i++) {
+    echo '<div class="news-cat"><div class="row"><div class=" col-md-12 col-sm-7"><div class="news-cat-caption"><h3><a href="news_detail.php?id='
+    .$result[$i]['id'].'">'.$result[$i]['title'].'</a></h3>';
+    echo '<p>'.$result[$i]['shortText'].'</p>';
+    echo '<p><a href="news_detail.php?id='.$result[$i]['id'].' class="button blue-btn">Подробнее</a></p></div></div></div></div>';
+}
+?>
 
-        <div id="main-slider">
-            <div id="slider" class="owl-carousel owl-theme" style="opacity: 1; display: block;">
-                        <div class="owl-item" style="width: 1140px;">
-                           	<div class="slide item">
-                                <div class="row">
-                                    <div class="col-md-5 col-lg-7 news-img">
-                                        <img src="/resources/IMG_3369-e1549484920105.jpg" class="attachment-600x800 size-600x800 wp-post-image" alt="IMG_3369" srcset="http://mik163.ru/wp-content/uploads/2019/02/IMG_3369-e1549484920105.jpg 1910w, http://mik163.ru/wp-content/uploads/2019/02/IMG_3369-e1549484920105-300x160.jpg 300w, http://mik163.ru/wp-content/uploads/2019/02/IMG_3369-e1549484920105-768x411.jpg 768w, http://mik163.ru/wp-content/uploads/2019/02/IMG_3369-e1549484920105-1024x547.jpg 1024w" sizes="(max-width: 600px) 100vw, 600px">                                    </div>
-                                    <div class="col-md-7 col-lg-5">
-                                        <div class="slide-caption">
-                                            <h3>🔥 Первые призы нашли своих победителей🔥</h3>
-                                            <p class="news-date">6 февраля 2019</p>
-                                            <p>Не так давно на сайте Молодежной избирательной комиссии Самарской области прошла интернет-олимпиада, посвященная 25-летию Конституции и избирательной системы Российской Федерации! Более 4 500 человек приняли участие в тестировании 1900 из которых стали победителями и призерами</p>
-
-                                            <a class="button blue-btn more-btn" href="http://mik163.ru/2019/02/06/%f0%9f%94%a5-%d0%bf%d0%b5%d1%80%d0%b2%d1%8b%d0%b5-%d0%bf%d1%80%d0%b8%d0%b7%d1%8b-%d0%bd%d0%b0%d1%88%d0%bb%d0%b8-%d1%81%d0%b2%d0%be%d0%b8%d1%85-%d0%bf%d0%be%d0%b1%d0%b5%d0%b4%d0%b8%d1%82%d0%b5%d0%bb-2/">Подробнее</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                    	</div>
-                    	<div class="owl-item" style="width: 1140px;">
-                    		<div class="slide item">
-                            	<div class="row">
-                                	<div class="col-md-5 col-lg-7 news-img">
-                                    	<img src="/resources/KLtRZgQfZKE.jpg" class="attachment-600x800 size-600x800 wp-post-image" alt="KLtRZgQfZKE" srcset="http://mik163.ru/wp-content/uploads/2019/01/KLtRZgQfZKE.jpg 1280w, http://mik163.ru/wp-content/uploads/2019/01/KLtRZgQfZKE-300x200.jpg 300w, http://mik163.ru/wp-content/uploads/2019/01/KLtRZgQfZKE-768x512.jpg 768w, http://mik163.ru/wp-content/uploads/2019/01/KLtRZgQfZKE-1024x682.jpg 1024w" sizes="(max-width: 600px) 100vw, 600px">
-                                	</div>
-                                    <div class="col-md-7 col-lg-5">
-                                        <div class="slide-caption">
-                                            <h3>25 января члены Молодежной избирательной комиссии Самарской области приняли участие во встрече с заместителем министра образования и науки Самарской области Сергеем Бурцевым на площадке молодежного лифта StrelkaHall.</h3>
-                                            <p class="news-date">29 января 2019</p>
-                                            <p>Сергей Александрович поздравил молодежный актив с Днем студента, рассказал о самых значимых региональных и федеральных студенческих проектах, таких как Студент Года, Студенческая весна, Молодежный форум «iВолга», отметил активную деятельность Молодежных совещательных структур региона, в том числе рассказал про Молодежную избирательную комиссию Самарской области.</p>
-
-                                            <a class="button blue-btn more-btn" href="http://mik163.ru/2019/01/29/25-%d1%8f%d0%bd%d0%b2%d0%b0%d1%80%d1%8f-%d1%87%d0%bb%d0%b5%d0%bd%d1%8b-%d0%bc%d0%be%d0%bb%d0%be%d0%b4%d0%b5%d0%b6%d0%bd%d0%be%d0%b9-%d0%b8%d0%b7%d0%b1%d0%b8%d1%80%d0%b0%d1%82%d0%b5%d0%bb%d1%8c%d0%bd/">Подробнее</a>
-                                        </div>
-                                    </div>
-                            	</div>
-                        	</div>
-                    	</div>
-                    	<div class="owl-item" style="width: 1140px;">
-                    		<div class="slide item">
-                                <div class="row">
-                                    <div class="col-md-5 col-lg-7 news-img">
-                                        <img src="/resources/V3q1Toun6SE.jpg" class="attachment-600x800 size-600x800 wp-post-image" alt="V3q1Toun6SE" srcset="http://mik163.ru/wp-content/uploads/2019/01/V3q1Toun6SE.jpg 960w, http://mik163.ru/wp-content/uploads/2019/01/V3q1Toun6SE-300x136.jpg 300w, http://mik163.ru/wp-content/uploads/2019/01/V3q1Toun6SE-768x347.jpg 768w" sizes="(max-width: 600px) 100vw, 600px">                                    </div>
-                                    <div class="col-md-7 col-lg-5">
-                                        <div class="slide-caption">
-                                            <h3>🔥🔥🔥 Сегодня 25 января, а значит мы готовы огласить полный список победителей и призеров интернет-олимпиады, посвященной 25-летию Конституции и избирательной системы Российской Федерации! 🔥🔥🔥</h3>
-                                            <p class="news-date">25 января 2019</p>
-                                            <p>✌🏻 Мы рады поделиться нашим невероятным результатом: в интернет-олимпиаде приняло участие почти 4500 участников! 😳 Из них 15 и более баллов набрали почти 1900 участников, которые гарантированно получат от нас памятный подарок – бласлет-флэшку! 🎁🎁🎁</p>
-
-                                            <a class="button blue-btn more-btn" href="http://mik163.ru/2019/01/25/%f0%9f%94%a5%f0%9f%94%a5%f0%9f%94%a5-%d1%81%d0%b5%d0%b3%d0%be%d0%b4%d0%bd%d1%8f-25-%d1%8f%d0%bd%d0%b2%d0%b0%d1%80%d1%8f-%d0%b0-%d0%b7%d0%bd%d0%b0%d1%87%d0%b8%d1%82-%d0%bc%d1%8b-%d0%b3%d0%be%d1%82/">Подробнее</a>
-                                        </div>
-                                    </div>
-                                </div>
-                        	</div>
-                    	</div>
-                    	<div class="owl-item" style="width: 1140px;">
-                    		<div class="slide item">
-                                <div class="row">
-                                    <div class="col-md-5 col-lg-7 news-img">
-                                        <img src="/resources/1-e1545007387243.jpg" class="attachment-600x800 size-600x800 wp-post-image" alt="конституция1" srcset="http://mik163.ru/wp-content/uploads/2018/12/1-e1545007387243.jpg 1488w, http://mik163.ru/wp-content/uploads/2018/12/1-e1545007387243-300x259.jpg 300w, http://mik163.ru/wp-content/uploads/2018/12/1-e1545007387243-768x664.jpg 768w, http://mik163.ru/wp-content/uploads/2018/12/1-e1545007387243-1024x885.jpg 1024w" sizes="(max-width: 600px) 100vw, 600px">                                    </div>
-                                    <div class="col-md-7 col-lg-5">
-                                        <div class="slide-caption">
-                                            <h3>Мы запустили новую интернет-олимпиаду с памятными подарками!</h3>
-                                            <p class="news-date">17 декабря 2018</p>
-                                            <p>Друзья, в ноябре мы провели интернет-олимпиаду для старшеклассников по избирательному праву. Мы получили&nbsp; отличный отклик от участников с пожеланиями и дальше проводить подобные мероприятия для всех желающих! Все мы знаем, что 12 декабря Конституция нашего государства отметила 25-летний юбилей! А вместе с ней и вся современная избирательная система России, основы которой были заложены Конституцией! Конечно, […]</p>
-
-                                            <a class="button blue-btn more-btn" href="http://mik163.ru/2018/12/17/%d0%bc%d1%8b-%d0%b7%d0%b0%d0%bf%d1%83%d1%81%d1%82%d0%b8%d0%bb%d0%b8-%d0%bd%d0%be%d0%b2%d1%83%d1%8e-%d0%b8%d0%bd%d1%82%d0%b5%d1%80%d0%bd%d0%b5%d1%82-%d0%be%d0%bb%d0%b8%d0%bc%d0%bf%d0%b8%d0%b0%d0%b4/">Подробнее</a>
-                                        </div>
-                                    </div>
-                                </div>
-							</div>
-                    	</div>
-                        <div class="owl-item" style="width: 1140px;">
-                        	<div class="slide item">
-                                <div class="row">
-                                    <div class="col-md-5 col-lg-7 news-img">
-                                        <img src="/resources/KQlpq4dlRyY.jpg" class="attachment-600x800 size-600x800 wp-post-image" alt="KQlpq4dlRyY" srcset="http://mik163.ru/wp-content/uploads/2018/12/KQlpq4dlRyY.jpg 960w, http://mik163.ru/wp-content/uploads/2018/12/KQlpq4dlRyY-300x225.jpg 300w, http://mik163.ru/wp-content/uploads/2018/12/KQlpq4dlRyY-768x576.jpg 768w" sizes="(max-width: 600px) 100vw, 600px">                                    </div>
-                                    <div class="col-md-7 col-lg-5">
-                                        <div class="slide-caption">
-                                            <h3>💥6 декабря в г. Сызрань прошёл отборочный этап Областной деловой игры «Молодежь в кабинетах власти» среди учащихся школ города Сызрани и округа. Игра помогает подрастающему поколению ознакомиться с деятельностью органов государственной власти и органов местного самоуправления Самарской области ☺</h3>
-                                            <p class="news-date">7 декабря 2018</p>
-
-                                            <a class="button blue-btn more-btn" href="http://mik163.ru/2018/12/07/%f0%9f%92%a56-%d0%b4%d0%b5%d0%ba%d0%b0%d0%b1%d1%80%d1%8f-%d0%b2-%d0%b3-%d1%81%d1%8b%d0%b7%d1%80%d0%b0%d0%bd%d1%8c-%d0%bf%d1%80%d0%be%d1%88%d1%91%d0%bb-%d0%be%d1%82%d0%b1%d0%be%d1%80%d0%be%d1%87%d0%bd/">Подробнее</a>
-                                        </div>
-                                    </div>
-                                </div>
-                        	</div>
-                </div>
-
-			</div>
-	    </div>
-
-                          <div class="main col-md-12">
-                  <h1>О МОЛОДЕЖНОЙ ИЗБИРАТЕЛЬНОЙ КОМИССИИ САМАРСКОЙ ОБЛАСТИ</h1>
-                  <p>Молодежная избирательная комиссия Самарской области является постоянно действующим совещательным и консультативным органом при Избирательной комиссии Самарской области. Главная задача комиссии &#8212; оказание содействия Избирательной комиссии Самарской области в деятельности по повышению правовой культуры молодых и будущих избирателей, формированию у молодежи активной гражданской позиции, осознанного интереса к избирательному праву и избирательному процессу, а также по обучению участников избирательного процесса и формированию кадрового резерва для избирательной системы.</p>
-<p><img class="size-full wp-image-147 aligncenter" src="/resources/news.png" alt="news" width="800" height="502" srcset="/resources/news.png 800w, /resources/news-300x188.png 300w, /resources/news-768x482.png 768w" sizes="(max-width: 800px) 100vw, 800px" /></p>
+         <!--div class="news-cat">
+          <div class="row">
+              <div class=" col-md-12 col-sm-7">
+                  <div class="news-cat-caption">
+                      <h3><a href="http://mik163.ru/2019/01/29/25-%d1%8f%d0%bd%d0%b2%d0%b0%d1%80%d1%8f-%d1%87%d0%bb%d0%b5%d0%bd%d1%8b-%d0%bc%d0%be%d0%bb%d0%be%d0%b4%d0%b5%d0%b6%d0%bd%d0%be%d0%b9-%d0%b8%d0%b7%d0%b1%d0%b8%d1%80%d0%b0%d1%82%d0%b5%d0%bb%d1%8c%d0%bd/">25 января члены Молодежной избирательной комиссии Самарской области приняли участие во встрече с заместителем министра образования и науки Самарской области Сергеем Бурцевым на площадке молодежного лифта StrelkaHall.</a></h3>
+                      <p>Сергей Александрович поздравил молодежный актив с Днем студента, рассказал о самых значимых региональных и федеральных студенческих проектах, таких как Студент Года, Студенческая весна, Молодежный форум «iВолга», отметил активную деятельность Молодежных совещательных структур региона, в том числе рассказал про Молодежную избирательную комиссию Самарской области.</p>
+                      <p><a href="http://mik163.ru/2019/01/29/25-%d1%8f%d0%bd%d0%b2%d0%b0%d1%80%d1%8f-%d1%87%d0%bb%d0%b5%d0%bd%d1%8b-%d0%bc%d0%be%d0%bb%d0%be%d0%b4%d0%b5%d0%b6%d0%bd%d0%be%d0%b9-%d0%b8%d0%b7%d0%b1%d0%b8%d1%80%d0%b0%d1%82%d0%b5%d0%bb%d1%8c%d0%bd/" class="button blue-btn">Подробнее</a></p>
+                  </div>
               </div>
-                      <!-- post navigation -->
+          </div>
+          </div>
 
-      </div>
+         <div class="news-cat">
+          <div class="row">
+              <div class=" col-md-12 col-sm-7">
+                  <div class="news-cat-caption">
+                      <h3><a href="http://mik163.ru/2019/01/25/%f0%9f%94%a5%f0%9f%94%a5%f0%9f%94%a5-%d1%81%d0%b5%d0%b3%d0%be%d0%b4%d0%bd%d1%8f-25-%d1%8f%d0%bd%d0%b2%d0%b0%d1%80%d1%8f-%d0%b0-%d0%b7%d0%bd%d0%b0%d1%87%d0%b8%d1%82-%d0%bc%d1%8b-%d0%b3%d0%be%d1%82/">🔥🔥🔥 Сегодня 25 января, а значит мы готовы огласить полный список победителей и призеров интернет-олимпиады, посвященной 25-летию Конституции и избирательной системы Российской Федерации! 🔥🔥🔥</a></h3>
+                      <p>✌🏻 Мы рады поделиться нашим невероятным результатом: в интернет-олимпиаде приняло участие почти 4500 участников! 😳 Из них 15 и более баллов набрали почти 1900 участников, которые гарантированно получат от нас памятный подарок – бласлет-флэшку! 🎁🎁🎁</p>
+                      <p><a href="http://mik163.ru/2019/01/25/%f0%9f%94%a5%f0%9f%94%a5%f0%9f%94%a5-%d1%81%d0%b5%d0%b3%d0%be%d0%b4%d0%bd%d1%8f-25-%d1%8f%d0%bd%d0%b2%d0%b0%d1%80%d1%8f-%d0%b0-%d0%b7%d0%bd%d0%b0%d1%87%d0%b8%d1%82-%d0%bc%d1%8b-%d0%b3%d0%be%d1%82/" class="button blue-btn">Подробнее</a></p>
+                  </div>
+              </div>
+          </div-->
   </section>
   <footer>
       <a id='to-top' href='javascript:click(0);'><img src='/resources/arrow-to-top.png'></a>
